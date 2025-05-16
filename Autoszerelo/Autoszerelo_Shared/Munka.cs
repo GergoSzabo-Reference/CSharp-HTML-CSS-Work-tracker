813bb59d308815020c2d2313f4f6ac9440a2d52b
szabohedergergo <157704096+szabohedergergo@users.noreply.github.com>
2025-05-16 18:17:40 +0200

    data validation

--- Autoszerelo/Autoszerelo_Shared/Munka.cs
+++ Autoszerelo/Autoszerelo_Shared/Munka.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Autoszerelo_Shared
 {
@@ -6,13 +7,27 @@ namespace Autoszerelo_Shared
     {
         public int MunkaId { get; set; }
         public int UgyfelId { get; set; }
-        
+
+        [Required(ErrorMessage = "A rendszám megadása kötelező.")]
+        [RegularExpression(@"[A-Za-z][A-Za-z][A-Za-z]-\d\d\d")]
         public string Rendszam { get; set; }
-        
+
+        [Required(ErrorMessage = "A gyártási év megadása kötelező.")]
+        [Range(1900, 2025, ErrorMessage = "A gyártási évnek 1900 és 2025 között kell lennie.")]
         public int GyartasiEv { get; set; }
+
+        [Required(ErrorMessage = "A munka kategória megadása kötelező.")]
         public MunkaKategoria Kategoria { get; set; }
+
+        [Required(ErrorMessage = "A hibaleírás megadása kötelező.")]
+        [StringLength(1000, ErrorMessage = "A hibaleírás legfeljebb 1000 karakterből állhat.")]
         public string HibaLeiras { get; set; }
+
+        [Required(ErrorMessage = "A hiba súlyosságának megadása kötleező.")]
+        [Range(1, 10, ErrorMessage = "A hiba súlyossága 1 és 10 közötti érték lehet.")]
         public int HibaSulyossaga { get; set; }
+
+        [Required(ErrorMessage = "A munka állapot megadása kötelező.")]
         public MunkaAllapot Allapot { get; set; }
     }
 }
--- Autoszerelo/Autoszerelo_Shared/Ugyfel.cs
+++ Autoszerelo/Autoszerelo_Shared/Ugyfel.cs
@@ -1,10 +1,22 @@
-﻿namespace Autoszerelo_Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autoszerelo_Shared
 {
     public class Ugyfel
     {
         public int UgyfelId { get; set; }
+
+        [Required(ErrorMessage = "Az ügyfél neve kötelező.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "A névnek legalább 2 és legfeljebb 100 karakternek kell lennie.")]
         public string Nev { get; set; }
+
+        [Required(ErrorMessage = "A lakcím kötelező.")]
+        [StringLength(200, ErrorMessage = "A lakcím legfeljebb 200 karaktert tartalmazhat.")]
         public string Lakcim { get; set; }
+
+        [Required(ErrorMessage = "Az email cím kötelező.")]
+        [EmailAddress(ErrorMessage = "Érvénytelen email cím formátum.")]
+        [StringLength(100, ErrorMessage = "Az email cím legfeljebb 100 karakter hosszú lehet.")]
         public string Email { get; set; }
     }
 }
